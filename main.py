12830cf20e4eca09bc1d1ef7dad26dec538d6c57
Caleb Kemere <ckemere@gmail.com>
2022-08-29 09:03:47 -0500

    Working pyglet system

--- main.py
+++ main.py
@@ -8,12 +8,12 @@
 # from panda3d.core import loadPrcFileData
 
 # Utilities
+import time
 from ctypes import c_int
 import pyglet
 from pyglet.window import key
 
 import zmq
-# import numpy as np
 import math
 import yaml
 from subprocess import check_output
@@ -25,7 +25,7 @@ import socket
 from ParametricShapes import makeCylinder, makePlane
 
 version = '1.1'
-
+DISPLAY_FPS = True
 
 class Model:
     def __init__(self, maze_config={}):
@@ -211,28 +211,29 @@ class Window(pyglet.window.Window):
     lock = False
     mouse_lock = property(lambda self:self.lock, setLock)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, synced_position, *args, **kwargs):
         display = pyglet.canvas.Display()
         screen = display.get_default_screen()
         self.screen_h_res = screen.width
         self.screen_v_res = screen.height
 
-        super().__init__(width=self.screen_h_res, height=self.screen_v_res, 
-                         style=pyglet.window.Window.WINDOW_STYLE_BORDERLESS,
-                         caption="PyRenderMaze", *args, **kwargs)
-        # super().__init__(fullscreen=True, caption="PyRenderMaze", *args, **kwargs)
+        # super().__init__(width=self.screen_h_res, height=self.screen_v_res,
+        #                 #  style=pyglet.window.Window.WINDOW_STYLE_BORDERLESS,
+        #                  caption="PyRenderMaze", *args, **kwargs)
+        super().__init__(fullscreen=True, caption="PyRenderMaze", *args, **kwargs)
+
+        self._synced_position = synced_position
 
         # Read YAML file
         with open("display_config.yaml", 'r') as stream:
             display_config = yaml.safe_load(stream)
 
-        # loadPrcFileData("", "fullscreen true") # causes some sort of bug where run loop doesn't start in Ubuntu
-
         self.keys = key.KeyStateHandler()
         self.push_handlers(self.keys)
         pyglet.clock.schedule(self.update)
 
-        self.fps_display = pyglet.window.FPSDisplay(self)
+        if DISPLAY_FPS:
+            self.fps_display = pyglet.window.FPSDisplay(self)
 
         self.mouseHeight = display_config.get('MouseEyeHeight', 3) # The mouse's eye position relative to the top of the wheel
         self.cameraHeight = 0 + self.mouseHeight # The height of the eye/camera relative to 0
@@ -253,17 +254,40 @@ class Window(pyglet.window.Window):
 
         self.display_regions = [int(phys_disp_region[0] / float(monitor_width) * h_res), # display region in pixels
                                 int(phys_disp_region[1] / float(monitor_height) * v_res),
-                                int(phys_disp_region[2] / float(monitor_width) * h_res),
+                                       int(phys_disp_region[2] / float(monitor_width) * h_res),
                                 int(phys_disp_region[3] / float(monitor_height) * v_res),]
 
 
         self.distance = display_config.get('MonitorDistance', 24) # default to 24 cm distance
 
+        self.z_clipping = display_config.get('ClippingDistance', 0.05) # default to clipping only the closest 5 mm
         self.max_clipping_distance = 1000
 
         self.screen_h_v_offsets = display_config.get('MonitorOffset', [0,0]) # default to no display center offset
 
         self.model = None
+        self.show_IP = True
+
+        IP = self.get_IP_address()
+
+        self.IP_label = pyglet.text.Label(IP,
+                          font_name='Arial',
+                          font_size=36,
+                          x=self.width, y=self.height,
+                          anchor_x='right', anchor_y='top')
+
+    def get_IP_address(self):
+        IP = None
+        if platform.system() == 'Linux':
+            IP = check_output(['hostname', '-I']).decode("utf-8","ignore")
+            while len(IP) < 7:
+                IP = check_output(['hostname', '-I']).decode("utf-8","ignore")
+        elif platform.system() == 'Darwin':
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
+                s.connect(('8.8.8.8', 80)) 
+                IP = s.getsockname()[0]
+        
+        return IP
 
     def construct_model(self, config={}):
         if self.model:
@@ -285,20 +309,38 @@ class Window(pyglet.window.Window):
     def on_draw(self):
         self.clear()
         pyglet.gl.glViewport(0, 0, self.width, self.height)
-        self.fps_display.draw()
-        pyglet.gl.glViewport(*self.display_regions)
 
+        if self.show_IP: # Show IP Address if model is not defined
+            pyglet.gl.glMatrixMode(pyglet.gl.GL_MODELVIEW)
+            pyglet.gl.glPushMatrix()
+            pyglet.gl.glLoadIdentity()
+
+            pyglet.gl.glMatrixMode(pyglet.gl.GL_PROJECTION)
+            pyglet.gl.glPushMatrix()
+            pyglet.gl.glLoadIdentity()
+            pyglet.gl.glOrtho(0, self.width, 0, self.height, -1, 1)
+
+            self.IP_label.draw()                
+
+            pyglet.gl.glPopMatrix()
+
+            pyglet.gl.glMatrixMode(pyglet.gl.GL_MODELVIEW)
+            pyglet.gl.glPopMatrix()
+
+        if DISPLAY_FPS:
+            self.fps_display.draw()
+
+        pyglet.gl.glViewport(*self.display_regions)
         # Projection
         pyglet.gl.glMatrixMode(pyglet.gl.GL_PROJECTION)
         pyglet.gl.glLoadIdentity()
 
-        zclip = 0.05
-        clip_scale = zclip/self.distance
+        clip_scale = self.z_clipping/self.distance
         pyglet.gl.glFrustum(clip_scale * (-self.screen_width/2 - self.screen_h_v_offsets[0]), 
                             clip_scale *  (  self.screen_width/2 - self.screen_h_v_offsets[0]), 
                             clip_scale *  (-self.screen_height/2 - self.screen_h_v_offsets[1]),
                             clip_scale * ( self.screen_height/2 - self.screen_h_v_offsets[1]),
-                            zclip, self.max_clipping_distance)
+                            self.z_clipping, self.max_clipping_distance)
         # pyglet.gl.gluPerspective(self.fov_h_v[1], self.aspect, 0.05, 1000)
         # Model
         pyglet.gl.glMatrixMode(pyglet.gl.GL_MODELVIEW)
@@ -308,7 +350,9 @@ class Window(pyglet.window.Window):
         pos = self.player.pos
         pyglet.gl.glRotatef(-rot[0],1,0,0)
         pyglet.gl.glRotatef(-rot[1],0,0,1)
-        pyglet.gl.glTranslatef(-pos[0], -pos[1], -pos[2])
+        # pyglet.gl.glTranslatef(-pos[0], -pos[1], -pos[2])
+        pyglet.gl.glTranslatef(0, -pos[1], 0)
+        # pyglet.gl.glTranslatef(0, self._synced_position.value, 0)
 
         self.model.draw()
 
@@ -345,29 +389,18 @@ from communications_process import start_commmunicator
 import multiprocessing
 from ctypes import c_int, c_double
 
-import sys
-
-def hello_world():
-    print(sys.stdout)
-    print('Hello world.')
-    return
-
 if __name__ == '__main__':
 
     config_queue = multiprocessing.Queue()
     synced_state = multiprocessing.Value(c_int)
     synced_position = multiprocessing.Value(c_double)
 
-    window = Window(resizable=True, vsync=False)
-
-    # maze_config_filename = "example-mazes/example_teleport.yaml"
-    # with open(maze_config_filename, "r") as stream:
-    #     maze_config = yaml.safe_load(stream)
-
-    window.construct_model({})
-    
-    synced_state.value = 1
     synced_position.value = 0.0
+
+    window = Window(synced_position, resizable=True, vsync=True)
+
+    window.construct_model({})    
+    synced_state.value = 1
 
     communicator_process = multiprocessing.Process(target=start_commmunicator, args=(synced_state, synced_position, config_queue))
     communicator_process.start()
@@ -383,6 +416,7 @@ if __name__ == '__main__':
             try:
                 window.construct_model(model_config)
                 synced_state.value = 1
+                window.show_IP = False # We successfully updated model, so stop displaying IP
             except:
                 synced_state = -1
 
@@ -390,7 +424,7 @@ if __name__ == '__main__':
             break
 
         if (window.has_exit):
-            print("Changing to exit state.")
+            print("PyRenderMaze exiting.")
             synced_state.value = -2
             window.close()
             break
